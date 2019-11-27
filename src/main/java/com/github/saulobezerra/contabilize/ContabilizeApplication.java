cfc35cec8b760b085c4a26da8c4703c8b66cb08e
Saulo Alves <sauloalves89@gmail.com>
2019-11-26 21:04:19 -0300

    criação do projeto contabilize

---
+++ src/main/java/com/github/saulobezerra/contabilize/ContabilizeApplication.java
@@ -0,0 +1,13 @@
+package com.github.saulobezerra.contabilize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/saulobezerra/contabilize/ContabilizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.saulobezerra.contabilize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
